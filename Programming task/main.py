5c208ee710a5f35fa6fba501c6e85100ddfb0ddc
Amr mamdouh <115363596+MAMDOUHjr@users.noreply.github.com>
2024-08-05 05:25:22 +0300

    Add files via upload

---
+++ Programming task/main.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def dominant_value(matrix):
+    eigenvalues, _ = np.linalg.eig(matrix)
+    dominant_eigenvalue = max(eigenvalues, key=abs)
+    return dominant_eigenvalue
+
+
+def inverseMatrix(matrix):
+    return np.linalg.inv(matrix)
+
+
+# def IdentityMatrix(matrix):
+#     # return np.dot(matrix, inverseMatrix(matrix)) note : something wrong with this function :(
+
+def IdentityMatrix(matrix):
+    n = matrix.shape[0]
+    identity = np.eye(n)
+    return identity
+
+
+matrix = np.array([[1, 2], [3, 4]])
+print("Dominant value: {:.2f}".format(dominant_value(matrix)))
+print("Inverse matrix: \n", inverseMatrix(matrix))
+print("Identity matrix: \n", IdentityMatrix(matrix))
